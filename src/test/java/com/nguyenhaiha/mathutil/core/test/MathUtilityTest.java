386c0743793e219150c6ed0cf835e7d4666bf656
thanlong421999 <thanlong421999@gmail.com>
2023-10-16 11:53:00 +0700

    update test script to use data-driven testing and to see green4rever)

--- src/test/java/com/nguyenhaiha/mathutil/core/test/MathUtilityTest.java
+++ src/test/java/com/nguyenhaiha/mathutil/core/test/MathUtilityTest.java
@@ -33,4 +33,21 @@ public class MathUtilityTest {
     public void verifyFactorialGivenRIghtArgument5ReturnOk() {
         assertEquals(120, MathUtility.getFactorial(5));
     }
+    
+    //3 testing trên ko đẹp, bốc mùi, bad smells vì chỉ khác nhau phần data
+    //TR0NG NGHỀ KIỂM THỬ CÓ 1 KỸ THUẬT ĐÓ LÀ TÁCH HẲN DATA KIỂM THỬ RA
+    //1 CHỖ, CHO DỄ NHÌN, DỄ QUẢN LÍ ĐC DATA THIẾU ĐỬ HAY KO
+   //SAU ĐÓ TA ĐƯA BỘ DÂT NÀY VÀO TRONG CÂU LỆNH SO SÁNH ASSERT()
+    //QUA CÁC THAM SỐ (PARAMETER)
+    //TỨC LÀ ASSERTEQUALS (EXPECTED , ACTUAL) LÀ 2 THAM SỐ
+    //ỨNG VỚI BỘ DATA TÁCH RA
+    //      120     5!
+    //      720     6!
+   //       24      4!
+    //              ...
+    
+    //CODE NHÌN GỌN HƠN, PHÁT HIỆN ĐC ĐỬ TEST DATA, TEST CASE
+    //KĨ THUẬT TÁCH DATA KIỂM THỬ RA KHỎI CÂU LỆNH SO SÁNH -> ĐC GỌI LÀ 
+    //DDT - DÂT DRIVEN TESTING - KIỂM THỬ THEO HƯỚNG TÁCH DATA RIÊNG LẺ
+    
 }
